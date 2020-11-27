21e9cc267862e8bfafe2c7e3da0dd31c3296d4a8
cesine <cesine@yahoo.com>
2020-11-26 22:15:29 -0500

    add extract test

---
+++ test/extract/yachtworld.spec.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const data = fs.readFileSync(path.join(__dirname, '../data/beneteau-oceanis-clipper-473-3101098.html')).toString();
+
+function extract({
+  keyword,
+  text,
+}) {
+  return text.match(keyword);
+}
+
+describe('yachtworld', () => {
+  it('should find the engine info', () => {
+    const result = extract({
+      keyword: /engine..............+[\n<]/gi,
+      text: data
+    });
+    expect(result.join(' ')).toContain('Yanmar');
+  });
+});
