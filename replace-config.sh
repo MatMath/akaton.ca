c2967818af280cfc0b0f7268139eb76b0ac8324d
Mathieu <mathieu.k.legault@gmail.com>
2023-10-14 09:45:26 -0400

    fix: apply author fix everywhere

--- replace-config.sh
+++ replace-config.sh
@@ -13,21 +13,18 @@ else
     echo "$placeholder was not empty $value"
 fi
 
-if [ -z "$directory" ]; then
-    if [ "$(uname)" == "Darwin" ]; then
-        sed -i '' "s/${placeholder}/${value}/" _config.yml
-    else
-        sed -i "s/${placeholder}/${value}/" _config.yml
-    fi
-    exit 0
-else 
-    find "$directory" -type f -print0 | while IFS= read -r -d $'\0' file; do
-        echo $file
-        if [ "$(uname)" == "Darwin" ]; then
-            sed -i '' "s;${placeholder};${value};" $file
-        else
-            sed -i "s;${placeholder};${value};" "$file"
-        fi
-        echo "Replaced ${placeholder} with ${value} in ${directory}"
-    done
+if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' "s/${placeholder}/${value}/" _config.yml
+else
+    sed -i "s/${placeholder}/${value}/" _config.yml
 fi
+
+find "$directory" -type f -print0 | while IFS= read -r -d $'\0' file; do
+    echo $file
+    if [ "$(uname)" == "Darwin" ]; then
+        sed -i '' "s;${placeholder};${value};" $file
+    else
+        sed -i "s;${placeholder};${value};" "$file"
+    fi
+    echo "Replaced ${placeholder} with ${value} in ${directory}"
+done
